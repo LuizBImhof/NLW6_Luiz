238eb039706dd11bec073d9a44e8d6581605b9be
LuizBImhof <luiz.imhof@gmail.com>
2021-06-21 22:30:13 -0300

    segundo dia NLW (Criando BD, Repositorio, Entidade, Controller, services, Rotas) usando TypeOrm sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
+import "reflect-metadata";
 import express from "express";
+import { router } from "./routes";
 
+import "./database"
 const app = express();
 
-app.get("/test", (request, response) => {
-    return response.send("Olá teste")
-})
+// app.get("/test", (request, response) => {
+//     return response.send("Olá teste")
+// })
 
-app.post ("/test-post", (request, response) => {
-    return response.send("Olá teste post") 
-})
+// app.post ("/test-post", (request, response) => {
+//     return response.send("Olá teste post") 
+// })
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => console.log("server is running"));
